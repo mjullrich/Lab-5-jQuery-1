5d9bec3167a16c4dfe9fe48bbad58df9729aae7d
Maryann Ullrich <maryann.ullrich@gmail.com>
2018-04-05 15:42:45 -0500

    Lab-5-jQuery-1-Final

--- DOM/app.js
+++ DOM/app.js
@@ -1,8 +1,19 @@
 
+document.addEventListener('DOMContentLoaded', function () {
+    createMessageButton();
+    createTextboxButton();
+    createTextboxDiv();
+    changeSquareColors();
+    changeParagraphColors();
+    createNameButton();
+    createNameDiv();
+    createFriendsButton();
+    createFriendsList();
+});
 
 // 1 - add div with button for nice message upon click
 
-document.addEventListener('DOMContentLoaded', function () {
+function createMessageButton() {
     let wrapper = document.createElement('div');
     wrapper.id = "my_centered_buttons";
     let button = document.createElement('button');
@@ -10,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function () {
     wrapper.appendChild(button);
     document.body.appendChild(wrapper);
     button.onclick = NiceMsg;
-});
+}
 
 function NiceMsg() {
     alert("Hope Your Having a Great Day Coding!!");
@@ -18,7 +29,7 @@ function NiceMsg() {
 
 // 2 - add div with button for textbox message upon click
 
-document.addEventListener('DOMContentLoaded', function () {
+function createTextboxButton() {
     let wrapper = document.createElement('div');
     wrapper.id = "my_centered_buttons2";
     let button = document.createElement('button');
@@ -26,10 +37,18 @@ document.addEventListener('DOMContentLoaded', function () {
     wrapper.appendChild(button);
     document.body.appendChild(wrapper);
     button.onclick = textboxMsg;
+}
 
-});
+function createTextboxDiv() {
+    let wrapper2 = document.createElement('div');
+    wrapper2.id = "emptyDiv3";
+    document.body.appendChild(wrapper2);
+}
 
 function textboxMsg() {
+
+    let wrapper2 = document.getElementById('emptyDiv3');
+
     if (!document.getElementById("centerBox")) {
         let wrapper = document.createElement('div');
         wrapper.id = "centerBox";
@@ -37,13 +56,15 @@ function textboxMsg() {
         x.setAttribute("type", "text");
         x.setAttribute("value", "Hello World!");
         wrapper.appendChild(x);
-        document.body.appendChild(wrapper);
+        wrapper2.appendChild(wrapper);
+
+        wrapper2.appendChild(wrapper);
     }
 }
 
 // 3 - add div square to change colors upon mouse over
 
-document.addEventListener('DOMContentLoaded', function () {
+function changeSquareColors() {
     let wrapper = document.createElement('div');
     let square = document.createElement('div');
     wrapper.id = "squareWrapper";
@@ -53,7 +74,7 @@ document.addEventListener('DOMContentLoaded', function () {
     document.body.appendChild(wrapper);
     document.getElementById("square").onmouseover = function () { mouseOver() };
     document.getElementById("square").onmouseout = function () { mouseOut() };
-});
+}
 
 function mouseOver() {
     document.getElementById("square").style.background = "red";
@@ -63,10 +84,9 @@ function mouseOut() {
     document.getElementById("square").style.background = "black";
 }
 
-
 // 4 - add div pararaph to chance colors upon click
 
-document.addEventListener('DOMContentLoaded', function () {
+function changeParagraphColors() {
     let wrapper = document.createElement('div');
     let paragraph = document.createElement('div');
     wrapper.id = "paraWrapper";
@@ -78,8 +98,7 @@ document.addEventListener('DOMContentLoaded', function () {
     wrapper.appendChild(paragraph);
     document.body.appendChild(wrapper);
     document.getElementById("paragraph").onclick = changeColors;
-
-});
+}
 
 function changeColors(event) {
     let paragraph = event.target;
@@ -89,32 +108,64 @@ function changeColors(event) {
 
 // 5 - add div with button when clicked shows my name
 
-document.addEventListener('DOMContentLoaded', function () {
+function createNameButton() {
     let wrapper = document.createElement('div');
     wrapper.id = "my_centered_buttons3";
     let button = document.createElement('button');
     button.innerText = 'Click For My Name';
     wrapper.appendChild(button);
     document.body.appendChild(wrapper);
-    button.onclick = NameMsg;  
-    
-});
+    button.onclick = NameMsg;
+}
 
-document.addEventListener('DOMContentLoaded', function () {
+function createNameDiv() {
     let wrapper2 = document.createElement('div');
     wrapper2.id = "emptyDiv";
     document.body.appendChild(wrapper2);
-   
-
-    
-});
+}
 
 function NameMsg() {
     if (!document.getElementById("myName")) {
-        let wrapper2 = document.getElementById("emptyDiv");
+        let wrapper2 = document.getElementById('emptyDiv');
         let newSpan = document.createElement('span');
         newSpan.id = "myName";
         newSpan.innerHTML = "Maryann Ullrich";
         wrapper2.appendChild(newSpan);
+    }
+}
+
+// 6 - list items added with button from an array
+
+function createFriendsButton() {
+    let wrapper = document.createElement('div');
+    wrapper.id = "my_centered_buttons4"
+    let button = document.createElement('button');
+    button.innerText = 'Click For My Friends';
+
+    wrapper.appendChild(button);
+    document.body.appendChild(wrapper);
+    button.onclick = addFriendName;
+}
+
+function createFriendsList() {
+    let wrapper2 = document.createElement('div');
+    wrapper2.id = "emptyDiv2";
+    let list = document.createElement('ul');
+    list.id = "friendList";
+
+    wrapper2.appendChild(list);
+
+    document.body.appendChild(wrapper2);
+}
+
+function addFriendName() {
+    let friendsArray = ["Tyler", "Stephen", "John", "Skyler", "Matt", "Ronnie", "Clayton", "Greg", "Adam", "Myles"];
+    let friendList = document.getElementById('friendList');
+    let childrenNum = friendList.children.length;
+
+    if (childrenNum < friendsArray.length) {
+        let item = document.createElement('li');
+        item.innerText = friendsArray[childrenNum];
+        friendList.appendChild(item);
     }
 }
